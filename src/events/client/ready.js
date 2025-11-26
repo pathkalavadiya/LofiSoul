83825519eb408fa2037f23ed7ab72d16d7cbcac4
pathkalavadiya <147802802+pathkalavadiya@users.noreply.github.com>
2025-11-26 12:16:46 +0530

    index files and events are complated

---
+++ src/events/client/ready.js
@@ -0,0 +1,15 @@
+const { Activity } = require("react");
+
+module.exports={
+    name:'ready',
+    once:true,
+    execute(client){
+       console.log(`Logged in as ${client.user.tag}`);
+    }
+
+    // client.user.setPresence({
+    //     activities:[{ name: "🎵 Lofi Music", type: 0 }], 
+    //     status: "online",
+    // });
+    
+}
